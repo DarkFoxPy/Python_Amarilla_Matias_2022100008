1f0a5fc168be2cc06bb793f3cf0f91775205fc07
MatiasAmarillaGr <matuamarillagallas@gmail.com>
2025-03-29 20:23:54 -0300

    Agregar Ejercicios clase 2

---
+++ Clase2/Ejercicio1.py
@@ -0,0 +1,15 @@
+'''
+Crear un programa que pida dos numero y obtenga como resultado
+cual de ellos es par o si ambos lo son
+'''
+numero1=int(input("Ingrese primer numero: "))
+numero2=int(input("Ingrese segundo numero: "))
+
+if((numero1%2==0) and(numero2%2==0)):
+    print("los numeros ",numero1 , "y", numero2, " son pares")
+elif ((numero1%2==0) and (numero2%2!=0)):
+    print("El numero ",numero1, "es par, y el numero ", numero2, "es impar")
+elif ((numero1%2!=0) and (numero2%2==0)):
+    print("El numero ",numero2, "es par, y el numero ", numero1, "es impar")
+else:
+    print("los numeros ",numero1 , "y", numero2, " son impares")
---
+++ Clase2/operadores.py
@@ -0,0 +1,30 @@
+'''
+suma
+'''
+n1=3
+n2=4
+print("la suma es:",n1+n2)
+
+'''
+resta
+'''
+n1=3
+n2=4
+print("la resta es:",n1-n2)
+
+'''
+Multi
+'''
+
+n1=3
+n2=4
+print("la multi es:",n1*n2)
+
+
+'''
+div
+'''
+
+n1=3
+n2=4
+print("la division es:",n1/n2)
