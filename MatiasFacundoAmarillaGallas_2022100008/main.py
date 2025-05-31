b7b5d12ef25978f01401b2b0597830309f3385e4
DarkFoxPy <matuamarillagallas@gmail.com>
2025-05-31 14:09:59 -0300

    import de cliente

--- MatiasFacundoAmarillaGallas_2022100008/main.py
+++ MatiasFacundoAmarillaGallas_2022100008/main.py
@@ -1,11 +1,11 @@
 
 from flask import Flask
-from login import login
+from cliente import cliente
 
 app = Flask(__name__)
 
-##servicios rest
-app.register_blueprint(login)
+
+app.register_blueprint(cliente)
 
 @app.route('/', methods=['GET'])
 def hello():
